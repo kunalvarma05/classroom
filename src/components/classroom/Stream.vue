18b97feb43a9cb975f838af28c5147443ed8f2cf
Kunal Varma <kunalvarma05@gmail.com>
2017-10-03 21:16:53 +0530

    Stream: Fetch Auth Token and start video

--- src/components/classroom/Stream.vue
+++ src/components/classroom/Stream.vue
@@ -1,33 +1,58 @@
 <template>
   <div class="stream-page">
-    <h1>Stream</h1>
-    <v-btn outline round v-if="!streamJoined" :loading='!accessToken' @click='joinStream' class="green--text">
-      <v-icon left>videocam</v-icon>
-      <span>Join Stream</span>
-    </v-btn>
+    <div class="stream-message">
+      <h1>Stream</h1>
+      <v-btn outline round v-if="!streamJoined" :loading='!accessToken' @click='joinStream' class="cyan--text">
+        <v-icon left>videocam</v-icon>
+        <span>Join Stream</span>
+      </v-btn>
+    </div>
   </div>
 </template>
 
 <script>
+  import Video from "../../lib/Video";
+
   export default {
     name: 'stream',
     data() {
       return {
         roomName: "general",
+        room: false,
         accessToken: false,
         streamJoined: false
       };
     },
     created() {
-
+      Video.generateAccessToken(this.$route.params.slug, this.roomName)
+        .then((token) => {
+          this.accessToken = token;
+        });
     },
     methods: {
       joinStream() {
-        this.streamJoined = true
-      },
-      connect(token) {
+        this.connect()
+          .then((room) => {
+            this.room = room;
 
+            this.attachRoomHandlers(room);
+          });
+      },
+      connect() {
+        return Video.connect(this.accessToken, this.roomName);
+      },
+      attachRoomHandlers(room) {
+        room.on('participantConnected', this.remoteParticipantConnected);
+      },
+      remoteParticipantConnected(participant) {
+        console.log(participant);
       }
     }
   }
 </script>
+
+<style lang="stylus">
+  .stream-page
+    display: flex;
+    align-items: center;
+</style>
