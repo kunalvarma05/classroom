1db8e3bcd503a2e6518c1dc2c4f5b2cedb57e764
Kunal Varma <kunalvarma05@gmail.com>
2017-10-05 18:05:23 +0530

    Toolbar account menu

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -6,6 +6,7 @@
       v-model="drawer"
       absolute
       mini-variant
+      floating
       clipped
       :dark="darkTheme"
     >
@@ -34,6 +35,56 @@
         <img src="../assets/images/logo.svg" alt="Classroom" class="logo" v-if="darkTheme">
         <img src="../assets/images/logo-blue.svg" alt="Classroom" class="logo" v-if="!darkTheme">
       </span>
+
+      <v-spacer></v-spacer>
+      <v-menu
+        offset-y
+        left
+        down
+        :close-on-content-click="false"
+        :nudge-width="150"
+        v-model="accountMenu"
+        class="account-menu"
+      >
+        <v-avatar class="white" slot="activator" size="36px">
+          <img :src="$currentUser.photoUrl" :alt="$currentUser.name">
+        </v-avatar>
+        <v-card>
+          <v-list>
+            <v-list-tile avatar @click="">
+              <v-list-tile-avatar>
+                <img :src="$currentUser.photoUrl" :alt="$currentUser.name">
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>{{$currentUser.name}}</v-list-tile-title>
+                <v-list-tile-sub-title v-if="!userIsTutor">Student</v-list-tile-sub-title>
+                <v-list-tile-sub-title v-if="userIsTutor">Tutor</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+          <v-divider></v-divider>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-switch v-model="darkTheme" color="primary"></v-switch>
+              </v-list-tile-action>
+              <v-list-tile-title>Dark Mode</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile @click="">
+              <v-list-tile-action>
+                <v-icon>settings</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-title>Settings</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile @click="">
+              <v-list-tile-action>
+                <v-icon>exit_to_app</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-title>Log out</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-menu>
     </v-toolbar>
     <main>
       <v-container fluid>
@@ -74,7 +125,8 @@
         drawer: true,
         notification: false,
         showNotification: false,
-        darkTheme: false
+        darkTheme: false,
+        accountMenu: false
       };
     },
     computed: {
@@ -115,4 +167,7 @@
     .toolbar
       .logo
         width: 120px;
+
+    .account-menu
+      padding: 0 15px;
 </style>
