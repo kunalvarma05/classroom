b3d995b83c79aa84bb20b972befd3a95bfaa7756
Kunal Varma <kunalvarma05@gmail.com>
2017-10-05 06:18:49 +0530

    Update courses page

--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -4,56 +4,44 @@
     <v-progress-circular indeterminate v-if='loading'></v-progress-circular>
     <v-container grid-list-md v-if='!loading'>
       <v-layout wrap>
-        <v-flex xs3 v-for='course in courses' :key='course.id'>
-          <form @submit.prevent="updateCourse(course)">
-            <v-card>
-              <v-card-title primary-title v-if="!course.visible">
-                <h6 class="mb-0">{{course.courseName}}</h6>
-              </v-card-title>
-              <v-card-text v-if="course.visible">
-                <v-text-field v-model='course.courseName' label="Name" required></v-text-field>
-              </v-card-text>
-              <v-divider></v-divider>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-
-                <v-btn icon @click="startSession(course.id, course.courseName)" v-if="!course.visible">
-                  <v-icon>launch</v-icon>
-                </v-btn>
-
-                <v-btn icon @click.native='toggleCourseVisible(course)' v-if="!course.visible">
-                  <v-icon>edit</v-icon>
-                </v-btn>
-                <v-btn icon @click.native='removeCourse(course)' v-if="!course.visible">
-                  <v-icon>delete</v-icon>
-                </v-btn>
-                <v-btn icon @click.native='updateCourse(course)' v-if="course.visible" type="submit">
-                  <v-icon>done</v-icon>
-                </v-btn>
-                <v-btn icon @click.native='toggleCourseVisible(course)' v-if="course.visible">
-                  <v-icon>cancel</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </form>
-        </v-flex>
         <v-flex xs3>
           <v-card>
             <v-card-text class="text-xs-center">
-              <v-btn fab small class="primary" @click='toggleAddCourseVisible' v-if='!addCourseVisible'>
+              <span v-if="!addingCourse && !addCourseVisible">Create Course</span>
+              <v-btn fab small class="primary" @click='addCourseVisible = true' v-if='!addCourseVisible'>
                 <v-icon>add</v-icon>
               </v-btn>
-              <v-text-field v-if='addCourseVisible' v-model='courseName' label="Name" required
+              <v-text-field v-if='addCourseVisible' v-model='courseName' label="Name" required :disabled="addingCourse"
                             @keyup.enter.prevent="addCourse"></v-text-field>
             </v-card-text>
             <v-divider v-if='addCourseVisible'></v-divider>
-            <v-card-actions v-if='addCourseVisible'>
+            <v-card-actions v-if="addCourseVisible && addingCourse">
+              <v-progress-circular small right indeterminate class="grey--text"></v-progress-circular>
+            </v-card-actions>
+            <v-card-actions v-if='addCourseVisible && !addingCourse'>
               <v-spacer></v-spacer>
               <v-btn icon @click.native='addCourse' type="submit">
                 <v-icon>done</v-icon>
               </v-btn>
-              <v-btn icon @click.native='toggleAddCourseVisible'>
+              <v-btn icon @click.native='addCourseVisible = false'>
                 <v-icon>cancel</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs3 v-for='course in courses' :key='course.id'>
+          <v-card>
+            <v-card-title primary-title>
+              <h6 class="mb-0">
+                {{course.name}}
+              </h6>
+            </v-card-title>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn flat small>
+                View
               </v-btn>
             </v-card-actions>
           </v-card>
@@ -65,67 +53,35 @@
 
 <script>
   import Firebase from '@/lib/Firebase'
-  import sessionService from '../store/Session';
+  import courseService from '../store/Course';
+  import VIcon from "../../node_modules/vuetify/src/components/VIcon/VIcon.vue";
 
   export default {
+    components: {VIcon},
     name: 'courses',
     created() {
-      Firebase.instance().database().ref('courses').child(this.$currentUser.id).on('value', (snapshot) => {
-        let courses = [];
-        let data = snapshot.val();
-
-        for (let key in data) {
-          let course = data[key];
-          course.id = key;
-          course.visible = false;
-          courses.push(course);
-        }
-
-        this.courses = courses;
-        this.loading = false;
-      });
+      this.fetchCourses();
     },
     data() {
       return {
         loading: true,
         courses: null,
-        courseName: null,
-        addCourseVisible: false
+        addCourseVisible: false,
+        addingCourse: false,
+        courseName: ""
       };
     },
     methods: {
-      startSession(course_id, course_name) {
-        sessionService.notifyStudents(this.$currentUser, {
-          id: course_id,
-          courseName: course_name
+      fetchCourses() {
+        courseService.getAllByTutorID(this.$currentUser.id).then((courses) => {
+          this.courses = courses;
+          this.loading = false;
         });
-        this.$router.push({name: 'classroom', params: {tutor_id: this.$currentUser.id, course_id: course_id}});
       },
       addCourse() {
-        Firebase.instance().database().ref('courses').child(this.$currentUser.id).push({
-          courseName: this.courseName
+        courseService.create(this.courseName, this.$currentUser.id).then((course) => {
+          this.courses.unshift(course);
         });
-        this.courseName = null;
-        this.toggleAddCourseVisible();
-      },
-      updateCourse(course) {
-        let updatedCourse = Object.assign({}, course);
-        delete updatedCourse.id;
-        delete updatedCourse.visible;
-        Firebase.instance().database().ref('courses').child(this.$currentUser.id).child(course.id).set(updatedCourse);
-      },
-      editCourse(course) {
-        this.courseName = course.courseName;
-      },
-      removeCourse(course) {
-        Firebase.instance().database().ref('courses').child(this.$currentUser.id).child(course.id).remove();
-      },
-      toggleCourseVisible(course) {
-        course.visible = !course.visible;
-        this.$set(this.courses, this.courses.indexOf(course), course);
-      },
-      toggleAddCourseVisible() {
-        this.addCourseVisible = !this.addCourseVisible;
       }
     }
   }
