71472d8e591ad6f253c678c235e40edb25a58f03
Kunal Varma <kunalvarma05@gmail.com>
2017-10-05 20:55:19 +0530

    Course Sessions sub-page

---
+++ src/pages/course/Sessions.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="course-sessions container fluid">
+    <h3 class="headline">Sessions</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'course-sessions',
+  }
+</script>
