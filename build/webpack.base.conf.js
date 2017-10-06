3adc17d67484c7fff9b1df9b94900ba0ef54a5ab
Kunal Varma <kunalvarma05@gmail.com>
2017-10-06 06:37:49 +0530

    Fix error

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,7 +34,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/vuetify/src')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
