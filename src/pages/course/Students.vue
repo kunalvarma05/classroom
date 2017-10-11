e78503a42daca8e91a7dab1c8d8e7f335bae3f20
Kunal Varma <kunalvarma05@gmail.com>
2017-10-12 00:27:56 +0530

    Add empty state to Course Students tab

--- src/pages/course/Students.vue
+++ src/pages/course/Students.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="course-students container fluid">
-    <h3 class="headline mb-3">
-      Students
-    </h3>
-    <v-list>
-      <v-list-tile avatar v-for="student in course.students" :key="student.id" @click="">
-        <v-list-tile-avatar>
-          <img :src="student.photoUrl" :alt="student.name"/>
-        </v-list-tile-avatar>
-        <v-list-tile-content>
-          <v-list-tile-title v-html="student.name"></v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
+    <h6 v-if="!hasStudents">
+      No Students have enrolled yet.
+    </h6>
+    <div v-if="hasStudents">
+      <h3 class="headline mb-3">
+        Students
+      </h3>
+      <v-list>
+        <v-list-tile avatar v-for="student in course.students" :key="student.id" @click="">
+          <v-list-tile-avatar>
+            <img :src="student.photoUrl" :alt="student.name"/>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title v-html="student.name"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </div>
   </div>
 </template>
 
 <script>
+  import Utils from "../../lib/Utils";
+
   export default {
     name: 'students',
     computed: {
       course() {
         return this.$parent.course;
+      },
+      hasStudents() {
+        return Utils.isArray(this.course.students) && this.course.students && this.course.students.length;
       }
     }
   }
